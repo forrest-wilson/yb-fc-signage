88c84dee8d5dc74fa4ee442386e6b7d48cadc08e
Forrest Wilson <forrest.wilson@me.com>
2017-11-06 14:49:34 +1300

    It works but no error validation

---
+++ app.js
@@ -0,0 +1,101 @@
+(function() {
+    var getEstimateButton = document.getElementById("getEstimate"),
+        signText = document.getElementById("signText");
+
+    var signColor;
+    var casedValue;
+
+    function validate() {
+        if (signText.value.length == 0) {
+            alert("Please enter some text for your sign");
+        } else {
+            return signText.value;
+        }
+    }
+
+    function letterCount() {
+        return signText.value.length;
+    }
+
+    function letterSize() {
+        if (document.querySelector('input[name="size"]:checked').value != null) {
+            var selectedSize = document.querySelector('input[name="size"]:checked').value;
+
+            switch (selectedSize) {
+                case "small":
+                    return 10;
+                    break;
+                case "large":
+                    return 20;
+                    break;
+                default: 
+                    console.log('please select a size');
+                    break;
+            }
+        }
+    }
+
+    function casing() {
+        if (document.querySelector('input[name="casing"]:checked').value != null) {
+            var upperOrLower = document.querySelector('input[name="casing"]:checked').value;
+
+            switch (upperOrLower) {
+                case "uppercase":
+                    casedValue = signText.value.toUpperCase();
+                    return "uppercase";
+                    break;
+                case "lowercase":
+                    casedValue = signText.value.toLowerCase();
+                    return "lowercase";
+                    break;
+                case "none":
+                    casedValue = signText.value;
+                    return "none";
+                    break;
+                default: 
+                    console.log('please select a casing');
+                    break;
+            }
+        }
+    }
+
+    function color() {
+        if (document.querySelector('input[name="color"]:checked').value != null) {
+            var colorSelection = document.querySelector('input[name="color"]:checked').value;
+
+            switch (colorSelection) {
+                case "black":
+                    signColor = "black";
+                    return 0;
+                    break;
+                case "red":
+                    signColor = "red";
+                    return 15;
+                    break;
+                case "blue":
+                    signColor = "blue";
+                    return 30;
+                    break;
+                default: 
+                    console.log('please select a color');
+                    break;
+            }
+        }
+    }
+
+    getEstimateButton.addEventListener("click", function(e) {
+        e.preventDefault();
+        var value = validate();
+        var letters = letterCount();
+        var size = letterSize();
+        var letterCasing = casing();
+        var colorSelection = color();
+        var totalCost;
+
+        totalCost = (letters * size) + colorSelection;
+
+        document.getElementById("signOutput").textContent = casedValue;
+        document.getElementById("signOutput").style.backgroundColor = signColor;
+        document.getElementById("cost").textContent = "$" + totalCost + ".00";
+    });
+})();
